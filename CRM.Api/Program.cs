f68751c5793373c9f9cebb59ba0d4b7a6fb5d7b5
WeCoders.nl <wecoders.nl@gmail.com>
2025-03-01 11:29:55 +0100

    Update user model

--- CRM.Api/Program.cs
+++ CRM.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using CRM.DataAccess;
+using CRM.Model.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -12,8 +14,6 @@ namespace CRM.Api
             var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found."); ;
 
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-
-            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
             // Add services to the container.
 
---
+++ CRM.Model/Enums/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace CRM.Model.Enums
+{
+    public enum Gender
+    {
+        [EnumMember(Value = "Male")]
+        Male,
+        [EnumMember(Value = "Female")]
+        Female,
+        [EnumMember(Value = "Other")]
+        Other
+    }
+}
