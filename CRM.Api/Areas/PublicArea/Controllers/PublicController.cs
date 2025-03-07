8682ca2c5251a6dab09029982ee9c63c64e21707
WeCoders.nl <WeCoders.nl@gmail.com>
2025-03-07 20:39:45 +0100

    Add public controller

---
+++ CRM.Api/Areas/PublicArea/Controllers/PublicController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace CRM.Api.Areas.PublicArea.Controllers
+{
+    [Area("PublicArea")]
+    [DisplayName("Public Controller")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class PublicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from Public Area");
+        }
+    }
+}
