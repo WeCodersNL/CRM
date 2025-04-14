eabff8e9f1fc6e2c9089ff38012ef7b1e9b13e4b
WeCoders.nl <WeCoders.nl@gmail.com>
2025-04-14 20:36:04 +0200

    Add JWT authentication to CRM API
    This commit introduces JWT (JSON Web Token) authentication by adding necessary package references and configuring authentication services. A new `TokenHandler` class is implemented to generate JWT tokens based on claims and configuration settings. Additionally, a token configuration section is added to `appsettings.json`, specifying the secret key, issuer, audience, and token expiry time.

--- CRM.Api/Program.cs
+++ CRM.Api/Program.cs
@@ -9,6 +9,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TokenHandler = CRM.Utility.TokenHandler;
 
 namespace CRM.Api
 {
@@ -29,6 +33,28 @@ namespace CRM.Api
             builder.Services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CRM API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
             });
 
             builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option =>
@@ -41,6 +67,28 @@ namespace CRM.Api
             builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
             builder.Services.Configure<EmailConfiguration>(builder.Configuration.GetSection("EmailConfiguration"));
             builder.Services.AddSingleton<IApplicationEmailSender, ApplicationEmailSender>();
+            builder.Services.Configure<TokenConfiguration>(builder.Configuration.GetSection("TokenConfiguration"));
+            builder.Services.AddSingleton<ITokenHandler, TokenHandler>();
+
+            builder.Services.AddAuthentication(optiones =>
+            {
+                optiones.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                optiones.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                optiones.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = builder.Configuration["TokenConfiguration:Audience"],
+                        ValidIssuer = builder.Configuration["TokenConfiguration:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenConfiguration:SecretKey"]!)),
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                    }
+                );
 
             var app = builder.Build();
 
---
+++ CRM.Utility/TokenHandler.cs
@@ -0,0 +1,31 @@
+﻿using CRM.Utility.IUtility;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CRM.Utility
+{
+    public class TokenHandler(IOptions<TokenConfiguration> tokenConfig) : ITokenHandler
+    {
+        private readonly TokenConfiguration _tokenConfig = tokenConfig.Value;
+
+        public string GenerateJwtToken(List<Claim> claims)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenConfig.SecretKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var tokenValidity = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_tokenConfig.TokenExpiry));
+
+            var token = new JwtSecurityToken(
+                        issuer: _tokenConfig.Issuer,
+                        audience: _tokenConfig.Audience,
+                        claims: claims,
+                        expires: tokenValidity,
+                        signingCredentials: signingCredentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
